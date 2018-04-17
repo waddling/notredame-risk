2cf1d3bb67bcca93d2f18b73bd7170dac22573e9
mzachlin <38113604+mzachlin@users.noreply.github.com>
2018-04-17 10:14:23 -0400

    Update playRisk.m

--- playRisk.m
+++ playRisk.m
@@ -11,11 +11,22 @@ RiskGame = Risk_Load_Board(RiskGame);
 
 troopNum = 5;
 
-% This example for loop will allow players 1-4 to place 5 troops using the aggressive strategy
+% This example for loop will allow players 1 and 2 to place 5 troops using the aggressive strategy
 
-for player = 1:4
+for player = 1:2
     
        
        RiskGame = troopPlaceAggressive(troopNum, player, RiskGame);
        
 end
+
+player = 3;
+
+RiskGame = troopPlaceDefensive(troopNum, player, RiskGame);
+
+player = 4;
+
+RiskGame = troopPlaceBalanced(troopNum, player, RiskGame);
+
+
+
