5af57a2d0892e73dfaaad1c9e7de82fa7ca7dde1
yun <13065713+theYunAndOnly@users.noreply.github.com>
2018-04-16 22:41:11 -0400

    Add files via upload

---
+++ changeBuildingColor.m
@@ -0,0 +1,62 @@
+function changeBuildingColor(buildingNumber, player)
+
+load('Risk_Structure.mat');
+
+[row, col, ch] = size(RiskGame(buildingNumber).image);
+
+figure(1);
+imshow(RiskGame(buildingNumber).image, 'InitialMagnification', 'fit');
+
+% Case per player
+switch player
+    % Changes building color to (1) light blue
+    case 1
+        for ir = 1:row
+            for ic = 1:col
+                if ((RiskGame(buildingNumber).image(ir,ic,1) > 100) && (RiskGame(buildingNumber).image(ir,ic,2) > 100)) && (RiskGame(buildingNumber).image(ir,ic,3) > 100)
+                    RiskGame(buildingNumber).image(ir,ic,1) = 211;
+                    RiskGame(buildingNumber).image(ir,ic,2) = 225;
+                    RiskGame(buildingNumber).image(ir,ic,3) = 255;
+                end
+            end
+        end
+    
+    % Changes building color to (2) light green
+    case 2
+        for ir = 1:row
+            for ic = 1:col
+                if ((RiskGame(buildingNumber).image(ir,ic,1) > 100) && (RiskGame(buildingNumber).image(ir,ic,2) > 100)) && (RiskGame(buildingNumber).image(ir,ic,3) > 100)
+                    RiskGame(buildingNumber).image(ir,ic,1) = 235;
+                    RiskGame(buildingNumber).image(ir,ic,2) = 255;
+                    RiskGame(buildingNumber).image(ir,ic,3) = 211;
+                end
+            end
+        end
+        
+    % Changes building color to (3) light red
+    case 3
+        for ir = 1:row
+            for ic = 1:col
+                if ((RiskGame(buildingNumber).image(ir,ic,1) > 100) && (RiskGame(buildingNumber).image(ir,ic,2) > 100)) && (RiskGame(buildingNumber).image(ir,ic,3) > 100)
+                    RiskGame(buildingNumber).image(ir,ic,1) = 255;
+                    RiskGame(buildingNumber).image(ir,ic,2) = 222;
+                    RiskGame(buildingNumber).image(ir,ic,3) = 211;
+                end
+            end
+        end
+        
+    % Changes building color to (4) light purple
+    case 4
+        for ir = 1:row
+            for ic = 1:col
+                if ((RiskGame(buildingNumber).image(ir,ic,1) > 100) && (RiskGame(buildingNumber).image(ir,ic,2) > 100)) && (RiskGame(buildingNumber).image(ir,ic,3) > 100)
+                    RiskGame(buildingNumber).image(ir,ic,1) = 244;
+                    RiskGame(buildingNumber).image(ir,ic,2) = 211;
+                    RiskGame(buildingNumber).image(ir,ic,3) = 255;
+                end
+            end
+        end
+end
+
+figure(2);
+imshow(RiskGame(buildingNumber).image, 'InitialMagnification', 'fit');
